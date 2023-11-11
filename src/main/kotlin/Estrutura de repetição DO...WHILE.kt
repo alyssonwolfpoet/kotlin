b0fb331ae3425c0b4dd83dc243a8af9d257b89cf
Alysson <alyssos@hotmail.com>
2023-11-11 15:13:47 -0300

    Estrutura de repetição DO...WHILE e leitura de dados

--- src/main/kotlin/Estrutura de repetição DO...WHILE.kt
+++ src/main/kotlin/Estrutura de repetição DO...WHILE.kt
@@ -4,4 +4,16 @@ fun main() {
         print("${i} ")
         i++
     }while (i != 10)
+
+    print("\n")
+    while (i==10){
+        print("Loop while")
+        i++
+    }
+
+    print("\n")
+    do {
+        print("Qual seu nome: ")
+        val value = readLine()
+    }while (value == "")
 }

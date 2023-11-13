75cd5d65d01ab7952ee8631b27ab0d31c6eb0a2e
Alysson <alyssos@hotmail.com>
2023-11-13 16:52:43 -0300

    2ª tarefa: Crie um programa que retorne a situação de um aluno, baseado em uma função que calcula a média de 4 notas (parâmetros) e verifique se essa média está: o Se uma nota for digitada acima de 10 ou menor que 0, imprima uma mensagem de erro (“Nota inexistente”) ao usuário e pare a execução do programa. o Aprovado: Maior ou igual a 7,0. o Recuperação: Maior ou igual a 4 e menor que 7. o Reprovado: Menor que 4

---
+++ src/main/kotlin/2ª tarefa.kt
@@ -0,0 +1,9 @@
+2ª tarefa: Crie um programa que retorne a situação de um aluno,
+baseado em uma função que calcula a média de 4 notas (parâmetros)
+e verifique se essa média está:
+o Se uma nota for digitada acima de 10 ou menor que 0, imprima
+uma mensagem de erro (“Nota inexistente”) ao usuário e pare
+a execução do programa.
+o Aprovado: Maior ou igual a 7,0.
+o Recuperação: Maior ou igual a 4 e menor que 7.
+o Reprovado: Menor que 4

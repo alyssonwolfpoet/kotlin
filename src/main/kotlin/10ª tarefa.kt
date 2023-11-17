a6ce52a9dc149d31588da574cdac321fa3fefa4d
Alysson <alyssos@hotmail.com>
2023-11-17 17:50:27 -0300

    10ª tarefa: Crie um programa que lê um vetor de Double de 10 elementos e pesquisa um número dentro desse vetor. Retorne “Elemento x achado” caso pertença ao vetor ou “Elemento x não achado” caso não pertença ao vetor.

---
+++ src/main/kotlin/10ª tarefa.kt
@@ -0,0 +1,13 @@
+fun vetor(double: Double): String {
+    var vetor = doubleArrayOf(1.0, 2.1, 3.2, 4.0, 5.0, 6.7, 7.9, 8.8, 9.0, 10.0)
+    for (i in 0 until vetor.size) {
+        if (vetor[i] == double) {
+            return "Elemento ${double} achado"
+        }
+    }
+    return "Elemento ${double} não achado"
+}
+
+fun main() {
+    println(vetor(10.0))
+}

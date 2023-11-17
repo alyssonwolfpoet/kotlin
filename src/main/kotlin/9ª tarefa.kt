42880e73f6136908dd05315afeb7281ff94adb13
Alysson <alyssos@hotmail.com>
2023-11-17 17:27:33 -0300

    9ª tarefa: Crie um algoritmo que converte horas em horas e minutos e minutos para horas e minutos. Atribua os valores a variáveis do tipo inteiras.

---
+++ src/main/kotlin/9ª tarefa.kt
@@ -0,0 +1,14 @@
+fun MinuToHoraminuto(minutos:Int){
+    var hora:Int = minutos/60
+    var minuto = minutos%60
+    println("${hora}:${minuto}")
+}
+fun HoraToHoraminuto(horas: Int){
+    var hora:Int = horas
+    var minuto = horas/60
+    println("${hora}:${minuto}")
+}
+fun main() {
+    MinuToHoraminuto(90)
+    HoraToHoraminuto(5)
+}

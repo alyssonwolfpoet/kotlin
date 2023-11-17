746faeaddab571676b85f902cec7f22a26b91c00
Alysson <alyssos@hotmail.com>
2023-11-17 15:13:02 -0300

    7ª tarefa: Crie uma função que leia uma variável do tipo inteira, verifique se essa variável contém um número primo ou não.

---
+++ src/main/kotlin/7ª tarefa.kt
@@ -0,0 +1,14 @@
+fun SePrimo(n :Int){
+    if (n%2 == 0)
+        println("Não é primo")
+    else(println("É primo"))
+}
+fun Lerint() {
+    print("digite Numero Inteiro: ")
+    //var n:Int = readLine()!!.toInt()
+    var n:Int = readln().toInt()
+    SePrimo(n)
+}
+fun main() {
+    Lerint()
+}

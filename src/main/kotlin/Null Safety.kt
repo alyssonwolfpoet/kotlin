b7a6467db061de34d08627f828c05a64baa02164
Alysson <alyssos@hotmail.com>
2023-11-19 22:41:43 -0300

    Null Safety

--- src/main/kotlin/Null Safety.kt
+++ src/main/kotlin/Null Safety.kt
@@ -1,3 +1,7 @@
 fun main() {
-
+    var str:String? = null
+    println(str?.length) // Kotlin identifica e assume o tratamento do erro
+    str = "Testando minha string"
+    println(str!!.length) // Eu assumo o erro
+    println("Testando meu code")
 }

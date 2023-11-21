8d806f6dce39d0857d16ddca02a038524f1c1429
Alysson <alyssos@hotmail.com>
2023-11-21 14:37:59 -0300

    Leitura de Dados com JAVA no Kotlin

---
+++ src/main/kotlin/Leitura de Dados com JAVA no Kotlin.kt
@@ -0,0 +1,40 @@
+import java.util.Scanner
+
+fun ler_int(){
+    val scanner = Scanner(System.`in`)
+    println("Digite um número inteiro: ")
+    var num = scanner.nextInt()
+    println("Você digitou ${num}")
+}
+fun ler_double(){ // Utilizar Vírgula
+    val scanner = Scanner(System.`in`)
+    println("Digite um número Double: ")
+    var num = scanner.nextDouble()
+    println("Você digitou ${num}")
+}
+fun ler_string(){
+    val scanner = Scanner(System.`in`)
+    println("Digite um número nome: ")
+    var num = scanner.next()
+    println("Você digitou ${num}")
+}
+fun ler_float(){ // Utilizar Vírgula
+    val scanner = Scanner(System.`in`)
+    println("Digite um número Float: ")
+    var num = scanner.nextFloat()
+    println("Você digitou ${num}")
+}
+fun main() {
+    ler_int()
+    ler_double()
+    ler_string()
+    ler_float()
+    ler_boolean()
+}
+fun ler_boolean(){
+    val scanner = Scanner(System.`in`)
+    println("Digite um valor Boolean: ")
+    var num = scanner.nextBoolean()
+    println("Você digitou ${num}")
+    scanner.close()
+}

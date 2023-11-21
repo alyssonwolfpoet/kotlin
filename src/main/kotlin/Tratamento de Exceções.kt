188b100f8f483ddcda71f6de257ca162c31a3256
Alysson <alyssos@hotmail.com>
2023-11-21 15:11:00 -0300

    Tratamento de Exceções

---
+++ src/main/kotlin/Tratamento de Exceções.kt
@@ -0,0 +1,22 @@
+import kotlin.math.sqrt
+
+fun main() {
+    val numerador = 10
+    val denominador = 0
+    try {
+        val res = (numerador/denominador)
+        println("Resultado da divisão $res")
+    }catch (e: ArithmeticException){
+        println("Erro: não existe divisão por zero!")
+    }
+    val numero = -4.0
+    try {
+        val res2 = sqrt(numero)
+        if (numero <0 ){
+            throw IllegalArgumentException()
+        }
+        println("Resultado da radiciação $res2")
+    }catch (e: IllegalArgumentException){
+        println("Não existe raiz real de número negativos")
+    }
+}

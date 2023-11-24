ba06c420aa9660579fdbf778779132b2c0878421
Alysson <alyssos@hotmail.com>
2023-11-23 21:25:39 -0300

    Any_Unit_Nothing

--- src/main/kotlin/Kotlin - Tratamento de Dados/Any_Unit_Nothing.kt
+++ src/main/kotlin/Kotlin - Tratamento de Dados/Any_Unit_Nothing.kt
@@ -2,8 +2,19 @@ package `Kotlin - Tratamento de Dados`
 
 fun main() {
     //Any Unit Nothing
-    a(10)
+    a(10.88888)
+    b()
+    //c()
 }
 fun a(valor:Any){
     println("Imprimindo o valor: $valor")
 }
+fun b():Unit{
+    println("Função sem retorno (void)")
+}
+fun c():Nothing{
+    // TODO: ("Função ainda a ser completada")
+    // A -> B
+}
+//fun save():Nothing{}
+//fun update():Nothing{}

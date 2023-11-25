6d508769610fbd427055a6badf6a708c298d461c
Alysson <alyssos@hotmail.com>
2023-11-25 18:32:24 -0300

    feat: tarefa 6

---
+++ src/main/kotlin/atividadesPraticas2/6ª tarefa.kt
@@ -0,0 +1,20 @@
+package atividadesPraticas2
+
+import java.io.File
+import java.io.FileNotFoundException
+import java.util.Scanner
+
+fun main() {
+
+    var arquivo = File("C:\\Users\\Alysson\\IdeaProjects\\kotlin\\src\\main\\kotlin\\atividadesPraticas2\\strings.1txt")
+    try {
+        Scanner(arquivo).use { l->
+            while (l.hasNext()){
+                println(l.next())
+            }
+            l.close()
+        }
+    }catch (e:FileNotFoundException){
+        println("erro na leitura do arquivo")
+    }
+}

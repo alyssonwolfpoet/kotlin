e163f59e57049aaecb5ebdce87ad49d474496bc2
Alysson <alyssos@hotmail.com>
2023-11-28 18:27:00 -0300

    :sparkles: feat: Dados no paradigma de orientação a objetos

--- src/main/kotlin/KotlinOrientaçãoaoObjeto/POO_base_leitura_de_dados.kt
+++ src/main/kotlin/KotlinOrientaçãoaoObjeto/POO_base_leitura_de_dados.kt
@@ -1,27 +1,37 @@
 package KotlinOrientaçãoaoObjeto
 
-class Pessoa(val ano_nas:Int,var nome:String){
+class Pessoa_(val ano_nas: Int, var nome: String) {
     var idade = 2023 - ano_nas
-    fun saudacao(){
+    fun saudacao() {
         println("Seja bem vindo ${this.nome}")
     }
-    fun acordar(x:Boolean = true){
-        if (x == true){
+
+    fun acordar(x: Boolean = true) {
+        if (x == true) {
             println("O ${this.nome} está acordado")
-        }else{
+        } else {
             println("O ${this.nome} está dormindo")
         }
     }
 }
 
 fun main() {
-    var pessoa:Pessoa = Pessoa(1998,"Max")
+    println("Digite o ano que você nasceu:")
+    var x = readlnOrNull()?.toInt() ?: 0
+    println("Digite o seu nome completo:")
+    var y = readln()
+    println("Você está acordado:")
+    //var d = readlnOrNull()?.toBoolean() ?: false
+    var d = readln()
+    var pessoa: Pessoa_ = Pessoa_(x, y)
     println(pessoa.ano_nas)
     println(pessoa.nome)
     println(pessoa)
     println("Você tem ${pessoa.idade} anos")
     pessoa.saudacao()
-    pessoa.acordar()
-    pessoa.acordar(true)
-    pessoa.acordar(false)
+    if (d == "sim" || d == "SIM" || d == "Sim") {
+        pessoa.acordar(true)
+    } else if (d == "não" || d == "NÃO" || d == "Não") {
+        pessoa.acordar(false)
+    }
 }

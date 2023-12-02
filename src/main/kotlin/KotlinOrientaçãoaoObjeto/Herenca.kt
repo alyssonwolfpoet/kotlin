78927e4a1f5b010fc888e78487003e9372e16b63
Alysson <alyssos@hotmail.com>
2023-12-02 13:45:11 -0300

    :sparkles: feat: println("Está ligado")

---
+++ src/main/kotlin/KotlinOrientaçãoaoObjeto/Herenca.kt
@@ -0,0 +1,32 @@
+package KotlinOrientaçãoaoObjeto
+
+open class Eletronico(marca: String) {
+    fun ligar() {
+            println("Está ligado")
+    }
+
+    fun desligar() {
+        println("Está desligado")
+    }
+    var modelo: String = "Teste de herança"
+}
+
+class Computador(marca: String) : Eletronico(marca) {
+    fun InstalarSoftware() {
+        println("Instalando softawere no computador")
+    }
+
+    fun Varredura() {
+        println("Realizando varredura no computador")
+    }
+}
+
+fun main() {
+    var c: Computador = Computador("Dell")
+    c.ligar()
+    c.desligar()
+    c.Varredura()
+    c.InstalarSoftware()
+    println(c.modelo)
+
+}

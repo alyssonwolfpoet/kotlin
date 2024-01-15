72ed78742e7b97b85dfa7fead199e63d0fbfeacf
Alysson <alyssos@hotmail.com>
2024-01-15 18:27:27 -0300

    feat: classe Animal

--- src/main/kotlin/AtividadesPráticas3/1ª tarefa.kt
+++ src/main/kotlin/AtividadesPráticas3/1ª tarefa.kt
@@ -4,9 +4,22 @@ class Animal(val nome:String,val tipo:String,val fala:String,val idade:Int) {
     fun  printAnimal(){
         println("Nome: ${this.nome}\nTipo: ${this.tipo}\nFala: ${this.fala}\nIdade: ${this.idade}")
     }
+
+    companion object {
+        var Nanimal:Int = 0
+        fun contaAnimal(Nanimal:Int){
+            println(" numeros de animais: ${++this.Nanimal}")
+        }
+        init {
+            contaAnimal(Nanimal)
+        }
+    }
+
 }
 
 fun main() {
     var a1 = Animal("cahorro","terreste","auau",15)
+    var a2 = Animal("gato","terreste","miau",10)
     a1.printAnimal()
+    a2.printAnimal()
 }

66538b7669961f1ecc0543857cbf714d78c1ec29
Alysson <alyssos@hotmail.com>
2024-01-16 21:14:22 -0300

    feat: classe Animal emplementada

--- src/main/kotlin/AtividadesPráticas3/1ª tarefa.kt
+++ src/main/kotlin/AtividadesPráticas3/1ª tarefa.kt
@@ -3,26 +3,32 @@ package AtividadesPráticas3
 class Animal(val nome:String,val tipo:String,val fala:String,val idade:Int) {
 
     fun  printAnimal(){
-        println("Nome: ${this.nome}\nTipo: ${this.tipo}\nFala: ${this.fala}\nIdade: ${this.idade}")
+        println("Nome: ${this.nome}\nTipo: ${this.tipo}\nFala: ${this.fala}\nIdade: ${this.idade}\n")
+    }
+    fun contaAnimal() {
+        println("numeros de animais: $Nanimal")
+    }
+    fun animalfalar(){
+        println("Animal falando: $fala")
     }
 
     companion object {
         var Nanimal: Int =0
-
-        fun contaAnimal(Nanimal:Int){
-            println(" numeros de animais: ${this.Nanimal}")
-        }
-        init {
-            Nanimal++
-            contaAnimal(Nanimal)
-        }
+    }
+    init {
+        Nanimal++
+        toString()
     }
 
+
+    override fun toString(): String {
+        return contaAnimal().toString() + printAnimal()
+    }
 }
 
 fun main() {
     var a1 = Animal("cahorro","terreste","auau",15)
     var a2 = Animal("gato","terreste","miau",10)
-    a1.printAnimal()
-    a2.printAnimal()
+    a1.animalfalar()
+    a2.animalfalar()
 }
